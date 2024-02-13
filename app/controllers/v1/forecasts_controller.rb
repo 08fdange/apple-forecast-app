ed01a09c21f8eacef71b7cbe11306e4bb14102cb
Frank D'Angelo <08fdange@gmail.com>
2024-02-12 19:01:49 -0500

    backend changes

--- app/controllers/v1/forecasts_controller.rb
+++ app/controllers/v1/forecasts_controller.rb
@@ -4,8 +4,9 @@ module V1
 
     def fetch_forecast
       zip_code = params[:zip_code]
+      days = params[:days] || 1
       if zip_code
-        forecast_data, cached = ForecastUpdateService.update_forecast(zip_code)
+        forecast_data, cached = ForecastUpdateService.update_forecast(zip_code, days)
         if forecast_data.present?
           render json: { forecast_data: forecast_data, cached: cached }, status: :ok
         else
