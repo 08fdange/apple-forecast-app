d79f60e577347d964d50fd257315d4ba088cb430
Frank D'Angelo <08fdange@gmail.com>
2024-02-15 09:42:44 -0500

    fix(UI): minor ui fixes

--- app/javascript/components/IPhoneWrapper.jsx
+++ app/javascript/components/IPhoneWrapper.jsx
@@ -16,21 +16,21 @@ const IPhoneWrapper = ({ children }) => {
     <div className="flex justify-center items-center h-screen overflow-hidden">
       <div className="relative h-[90vh] max-h-[800px] w-auto">
         <img src={imageUrl} alt="iphone-15" className="h-full w-auto" />
-        <div className="absolute z-20 top-[22px] left-[38%] w-16 h-7 bg-black rounded-full" />
+        <div className="absolute z-20 top-[22px] left-[37%] w-16 h-7 bg-black rounded-full" />
         <div className="absolute flex items-center justify-center z-20 top-[22px] right-[37%] w-7 h-7 bg-black rounded-full">
           <div className="w-[12px] h-[12px] bg-gray-900 rounded-full flex items-center justify-center">
             <div className="w-2 h-2 bg-black rounded-full"/>
           </div>
         </div>
         <div 
-          className="absolute z-0 top-[13px] bottom-[12px] left-[16px] right-[16px] rounded-[46px]"
+          className="absolute z-0 top-[12px] bottom-[11px] left-[15px] right-[15px] rounded-[44px]"
           style={{
             backgroundImage: `url(${backgroundUrl})`,
             backgroundPosition: 'center',
           }}
         />
         <div className="absolute z-10 top-0 left-0 right-0 bottom-0 flex justify-center items-center">
-          <div className="w-[94%] h-[97%] rounded-t-[58px] rounded-b-[53px] overflow-scroll p-4">
+          <div className="w-[94%] h-[97%] rounded-t-[58px] rounded-b-[53px] overflow-scroll no-scrollbar p-4">
             {children}
           </div>
         </div>
